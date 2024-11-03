27b697e8cb34ff7242565931bc4dec31b55be15b
Nurul Azim Sakib <sakibruns@gmail.com>
2024-11-04 03:49:03 +0600

    Home section responsive added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./src/**/*.{html,js}", "./*.html"],
   theme: {
     extend: {
-      
+      fontFamily: {
+        inter: ['Inter', 'sans-serif'],
+      },
+      colors: {
+        customColor: '#F9B800',
+      },
     },
   },
   plugins: [],
